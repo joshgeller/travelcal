7bac4adacf5e0ac246112dbbadded94a363bd9b2
emilyls <snyderem@oregonstate.edu>
2016-07-08 16:43:14 -0700

    form improvements

--- client/app/new-item/new-item.controller.js
+++ client/app/new-item/new-item.controller.js
@@ -11,9 +11,20 @@
 
   function NewItemController($http) {
     var vm = this;
-    vm.email = '';
-    vm.password = '';
-
+      vm.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      vm.selected = [];
+      vm.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+            list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+      };
+      vm.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+      }
   }
 
 })();
