327522676b8bac25f7feed840a1fcc2a6e5d21b3
jensbodal <jensbodal@gmail.com>
2016-07-09 12:47:40 -0700

    using ngMdIcons

--- client/app/core/core.module.js
+++ client/app/core/core.module.js
@@ -5,6 +5,7 @@
     .module('travelcal.core', [
       'ui.router',
       'ngMaterial',
+      'ngMdIcons',
       'ngStorage'
     ]);
 })();
--- client/app/nav/nav.controller.js
+++ client/app/nav/nav.controller.js
@@ -10,7 +10,16 @@
 
   function NavController() {
     var vm = this;
+    vm.openMenu = openMenu;
     vm.pageTitle = 'Dynamic Page Title';
+    
+    function openMenu($mdOpenMenu, ev) {
+      console.log("OPEN");
+      console.log(ev);
+      console.log($mdOpenMenu);
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    }
   }
 
 })();
