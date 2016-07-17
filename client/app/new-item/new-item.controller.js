e6c1cd10292a150d5b693fe225e782412c0f4dfb
emilyls <snyderem@oregonstate.edu>
2016-07-17 15:06:55 -0700

    prepopulated times now working, fixed bug with the end date validation

--- client/app/new-item/new-item.controller.js
+++ client/app/new-item/new-item.controller.js
@@ -11,13 +11,76 @@
 
     function NewItemController($http, $setPristine) {
         var vm = this;
-        vm.activity = {};
+        var edit = true;
         vm.newItemForm = {};
-        vm.activity.currency = '';
-
         vm.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
         vm.selected = [];
+
+        if (edit) {
+            vm.title = "Edit Activity"
+            vm.activity = {
+                "name": "Modern Art Museum",
+                "cost": 25,
+                "currency": "usd",
+                "days": {
+                    "Monday": {
+                        "start": "1970-01-01T08:32:00.000Z",
+                        "end": "1970-01-01T08:32:00.000Z",
+                        "open": true
+                    },
+                    "Tuesday": {
+                        "start": null,
+                        "end": null,
+                        "open": true
+                    },
+                    "Wednesday": {
+                        "start": null,
+                        "end": null,
+                        "open": true
+                    },
+                    "Thursday": {
+                        "start": null,
+                        "end": null,
+                        "open": true
+                    },
+                    "Friday": {
+                        "start": null,
+                        "end": null,
+                        "open": true
+                    },
+                    "Saturday": {
+                        "start": null,
+                        "end": null
+                    },
+                    "Sunday": {
+                        "start": null,
+                        "end": null
+                    }
+                },
+                "start": "2016-07-17T07:00:00.000Z",
+                "end": "2016-07-10T07:00:00.000Z",
+                "url": "moma.org",
+                "quantity": 2,
+                "notes": "Recommended"
+            };
+            vm.activity.start = new Date(vm.activity.start);
+            vm.activity.end = new Date(vm.activity.end);
+            vm.activity.days.Monday.start = new Date(vm.activity.days.Monday.start);
+            vm.activity.days.Monday.end = new Date(vm.activity.days.Monday.end);
+            var days = Object.getOwnPropertyNames(vm.activity.days);
+            vm.result = days;
+            days.forEach(function(value, index, array) {
+                if (vm.activity.days[value].open) {
+                    vm.selected.push(value);
+                }
+            });
+        }
+        else {
+            vm.title = "New Activity"
+            vm.activity = {};
+            vm.activity.currency = '';
+        }
+
         vm.toggle = function (item, list) {
             var idx = list.indexOf(item);
             if (idx > -1) {
@@ -53,7 +116,7 @@
             return;
         }
 
-        vm.result = [];
+//        vm.result = [];
         vm.submit = function () {
             var response = $http.post('/SOMETHING', vm.activity)
     //        response.success(function(data, status, headers, config) {
