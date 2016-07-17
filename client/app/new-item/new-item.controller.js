45f3c70b84893bc116f0bb9a992b3d36fb9146f8
emilyls <snyderem@oregonstate.edu>
2016-07-17 11:28:00 -0700

    some changes?

--- client/app/new-item/new-item.controller.js
+++ client/app/new-item/new-item.controller.js
@@ -14,6 +14,113 @@
         vm.activity = {};
 
         vm.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+        vm.periods =  [{
+ 			"close": {
+ 				"day": 0,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468216800000
+ 			},
+ 			"open": {
+ 				"day": 0,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468764000000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 1,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468303200000
+ 			},
+ 			"open": {
+ 				"day": 1,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468245600000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 2,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468389600000
+ 			},
+ 			"open": {
+ 				"day": 2,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468332000000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 3,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468476000000
+ 			},
+ 			"open": {
+ 				"day": 3,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468418400000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 4,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468562400000
+ 			},
+ 			"open": {
+ 				"day": 4,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468504800000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 5,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468648800000
+ 			},
+ 			"open": {
+ 				"day": 5,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468591200000
+ 			}
+ 		}, {
+ 			"close": {
+ 				"day": 6,
+ 				"time": "2300",
+ 				"hours": 23,
+ 				"minutes": 0,
+ 				"nextDate": 1468735200000
+ 			},
+ 			"open": {
+ 				"day": 6,
+ 				"time": "0700",
+ 				"hours": 7,
+ 				"minutes": 0,
+ 				"nextDate": 1468677600000
+ 			}
+ 		}];
         vm.selected = [];
         vm.toggle = function (item, list) {
             var idx = list.indexOf(item);
