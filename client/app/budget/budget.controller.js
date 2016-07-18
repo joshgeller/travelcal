88b82fefd3d72570d94eae602aa702ad630322a5
emilyls <snyderem@oregonstate.edu>
2016-07-17 17:25:21 -0700

    added currency conversion to budget

--- client/app/budget/budget.controller.js
+++ client/app/budget/budget.controller.js
@@ -3,24 +3,112 @@
 
   angular
     .module('travelcal.budget')
-    .controller('BudgetController', BudgetController);
+    .controller('BudgetController', BudgetController)
+    .service('CurrencyService', CurrencyService);
+
+
+  function CurrencyService($http) {
+    delete $http.defaults.headers.common['X-Requested-With'];
+    this.getData = function(callbackFunc) {
+        return $http({
+            method:'GET',
+            url: 'http://api.fixer.io/latest?base=USD'
+        }).success(function(data) {
+            callbackFunc(data);
+        }).error(function() {
+//             TODO fix this
+            alert("error");
+        });
+    }
+  }
 
   BudgetController.$inject = [
-    '$http'
+    '$http',
+    '$location',
+    'CurrencyService'
   ];
 
-  function BudgetController($http) {
+
+
+
+  function BudgetController($http, $location, CurrencyService) {
     var vm = this;
-    vm.budget = [{name: 'Art Museum', start: '9/28/2016', end: '9/28/2016', reps: 1, cost: 7.00, currency: 'usd'},
-                     {name: 'AirBnB', start: '9/28/2016', end: '10/10/2016', reps: 12, cost: 67.00, currency: 'usd', url: 'https://www.airbnb.com/rooms/2518456'},
-                     {name: 'Taxi', start: '9/29/2016', end: '10/1/2016', reps: 3, cost: 25.00, currency: 'usd'}]
+    vm.currencies = null;
+    vm.baseCurr = "USD";
     vm.total = 0;
-//    vm.register = function () {
-//      return $http.post('/api/v1/accounts/', {
-//        password: vm.password,
-//        email: vm.email
-//      });
-//    }
+    vm.budget = [
+                 {  "id": 1234,
+                    "name": "Modern Art Museum",
+                    "cost": 25,
+                    "currency": "AUD",
+                    "days": {
+                        "Monday": {
+                            "start": null,
+                            "end": null,
+                            "open": true
+                        },
+                        "Tuesday": {
+                            "start": null,
+                            "end": null,
+                            "open": true
+                        },
+                        "Wednesday": {
+                            "start": null,
+                            "end": null,
+                            "open": true
+                        },
+                        "Thursday": {
+                            "start": null,
+                            "end": null,
+                            "open": true
+                        },
+                        "Friday": {
+                            "start": null,
+                            "end": null,
+                            "open": true
+                        },
+                        "Saturday": {
+                            "start": null,
+                            "end": null
+                        },
+                        "Sunday": {
+                            "start": null,
+                            "end": null
+                        }
+                    },
+                    "start": "2016-07-17T07:00:00.000Z",
+                    "end": "2016-07-10T07:00:00.000Z",
+                    "url": "http://www.moma.org",
+                    "quantity": 2,
+                    "notes": "Recommended"
+                },
+                {  "id": 1111,
+                    "name": "AirBnB",
+                    "cost": 117,
+                    "currency": "CAD",
+                    "start": "2016-07-17T07:00:00.000Z",
+                    "end": "2016-07-18T07:00:00.000Z",
+                    "quantity": 1,
+                },
+                {  "id": 2222,
+                    "name": "Taxi",
+                    "cost": 25,
+                    "currency": "DKK",
+                    "start": "2016-07-19T07:00:00.000Z",
+                    "end": "2016-07-19T07:00:00.000Z",
+                    "quantity": 1,
+                }
+              ]
+    CurrencyService.getData(function(dataResponse) {
+        vm.currencies = dataResponse;
+        for (var item in vm.budget) {
+            vm.total += vm.budget[item].cost * vm.budget[item].quantity * vm.currencies.rates[vm.budget[item].currency];
+        }
+    })
+    vm.edit = function (id) {
+//        $location.url('#/new-item?edit=true&id=' + id)
+        $location.path('#/new-item')
+    }
   }
 
 })();
