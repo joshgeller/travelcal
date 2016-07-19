324800031ed44132df26e405914fce5c464cc3c1
emilyls <snyderem@oregonstate.edu>
2016-07-18 21:16:58 -0700

    not working

--- client/app/budget/budget.controller.js
+++ client/app/budget/budget.controller.js
@@ -39,13 +39,16 @@
   }
 
   BudgetController.$inject = [
+    '$scope',
+    '$mdDialog',
+    '$mdMedia',
     '$http',
     '$location',
     'CurrencyService'
   ];
 
 
-  function BudgetController($http, $location, CurrencyService) {
+  function BudgetController($scope, $mdDialog, $mdMedia, $http, $location, CurrencyService) {
     var vm = this;
     vm.currencies = null;
     vm.baseCurrency = "USD";
@@ -169,6 +172,51 @@
             return (currency.value.indexOf(uppercaseQuery) === 0);
         }
     }
+
+  $scope.status = '  ';
+  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+
+
+  $scope.showAdvanced = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'static/app/new-item/new-item.index.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+    });
+
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+
+  };
+
   }
 
 })();
+
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
