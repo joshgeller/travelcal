b90624f4d245ca7f95afdf5353f27cb4d012bc93
emilyls <snyderem@oregonstate.edu>
2016-07-19 18:50:06 -0700

    reformatting

--- client/app/budget/budget.controller.js
+++ client/app/budget/budget.controller.js
@@ -1,221 +1,151 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('travelcal.budget')
-    .controller('BudgetController', BudgetController)
-    .service('CurrencyService', CurrencyService);
+    angular
+        .module('travelcal.budget')
+        .controller('BudgetController', BudgetController)
+        .service('CurrencyService', CurrencyService);
 
 
-  function CurrencyService($http) {
-    delete $http.defaults.headers.common['X-Requested-With'];
+    function CurrencyService($http) {
+        delete $http.defaults.headers.common['X-Requested-With'];
+        var service = {};
+        service.getRates = getRates;
+        service.getCurrencies = getCurrencies;
+        return service;
 
-    var service = {};
-    service.getRates = getRates;
-    service.getCurrencies = getCurrencies;
-    return service;
+        function getRates(base) {
+            return $http({
+                method:'Get',
+                url:'http://api.fixer.io/latest?base=' + base
+            }).success(function(data) {
+                return data;
+            }).error(function() {
+                console.log("error");
+            })
+        }
 
-    function getRates(base) {
-        return $http({
-            method:'Get',
-            url:'http://api.fixer.io/latest?base=' + base
-        }).success(function(data) {
-            return data;
-        }).error(function() {
-            console.log("error");
-        })
+        function getCurrencies() {
+            return $http({
+                method:'Get',
+                url:'http://api.fixer.io/latest'
+            }).success(function(data) {
+                return data;
+            }).error(function() {
+                console.log("error");
+            })
+        }
     }
 
-    function getCurrencies() {
-        return $http({
-            method:'Get',
-            url:'http://api.fixer.io/latest'
-        }).success(function(data) {
-            return data;
-        }).error(function() {
-            console.log("error");
-        })
-    }
-  }
-
-  BudgetController.$inject = [
-    '$scope',
-    '$mdDialog',
-    '$mdMedia',
-    '$http',
-    '$location',
-    'CurrencyService'
-  ];
+    BudgetController.$inject = [
+        '$scope',
+        '$mdDialog',
+        '$mdMedia',
+        '$http',
+        '$location',
+        'CurrencyService'
+    ];
 
 
-  function BudgetController($scope, $mdDialog, $mdMedia, $http, $location, CurrencyService) {
-    var vm = this;
-    vm.currencies = null;
-    vm.baseCurrency = "USD";
-    vm.total = 0;
-    vm.budget = [
-             {  "id": 1234,
-                "title": "Modern Art Museum",
-                "cost": 25,
-                "currency": "AUD",
-                "days": {
-                    "Monday": {
-                        "start": null,
-                        "end": null,
-                        "open": true
-                    },
-                    "Tuesday": {
-                        "start": null,
-                        "end": null,
-                        "open": true
-                    },
-                    "Wednesday": {
-                        "start": null,
-                        "end": null,
-                        "open": true
-                    },
-                    "Thursday": {
-                        "start": null,
-                        "end": null,
-                        "open": true
-                    },
-                    "Friday": {
-                        "start": null,
-                        "end": null,
-                        "open": true
-                    },
-                    "Saturday": {
-                        "start": null,
-                        "end": null
-                    },
-                    "Sunday": {
-                        "start": null,
-                        "end": null
+    function BudgetController($scope, $mdDialog, $mdMedia, $http, $location, CurrencyService) {
+        var vm = this;
+        vm.currencies = null;
+        vm.baseCurrency = "USD";
+        vm.total = 0;
+        vm.budget = [];
+
+        CurrencyService.getCurrencies()
+            .then(function success(response) {
+                vm.currencies = response.data;
+                vm.currencyNames = Object.getOwnPropertyNames(vm.currencies.rates);
+                vm.currencyNames.push(response.data.base);
+                vm.currencyNames.sort();
+                vm.currencyAutoComplete = [];
+                for (var i = 0; i < vm.currencyNames.length; i++) {
+                    var newCurrency = {value:vm.currencyNames[i], display:vm.currencyNames[i]};
+                    vm.currencyAutoComplete.push(newCurrency);
+                    if (newCurrency.value == "USD") {
+                        vm.updateCurrency(newCurrency.value);
                     }
-                },
-                "startsAt": "2016-07-17T07:00:00.000Z",
-                "endsAt": "2016-07-17T07:00:00.000Z",
-                "url": "http://www.moma.org",
-                "quantity": 2,
-                "notes": "Recommended"
-            },
-            {  "id": 1111,
-                "title": "AirBnB",
-                "cost": 117,
-                "currency": "CAD",
-                "startsAt": "2016-07-17T07:00:00.000Z",
-                "endsAt": "2016-07-18T07:00:00.000Z",
-                "quantity": 1,
-            },
-            {  "id": 2222,
-                "title": "Taxi",
-                "cost": 25,
-                "currency": "DKK",
-                "startsAt": "2016-07-19T07:00:00.000Z",
-                "endsAt": "2016-07-19T07:00:00.000Z",
-                "quantity": 1,
-            }
-          ];
-
-    CurrencyService.getCurrencies()
-        .then(function success(response) {
-            vm.currencies = response.data;
-            vm.currencyNames = Object.getOwnPropertyNames(vm.currencies.rates);
-            vm.currencyNames.push(response.data.base);
-            vm.currencyNames.sort();
-            vm.currencyAutoComplete = [];
-            for (var i = 0; i < vm.currencyNames.length; i++) {
-                var newCurrency = {value:vm.currencyNames[i], display:vm.currencyNames[i]};
-                vm.currencyAutoComplete.push(newCurrency);
-                if (newCurrency.value == "USD") {
-                    vm.updateCurrency(newCurrency.value);
                 }
+            }, function error(response) {
+                console.log(response);
+        });
+
+        vm.updateCurrency = function updateCurrency(newBase){
+            if (newBase) {
+                CurrencyService.getRates(newBase)
+                    .then(function success(response) {
+                        vm.baseCurrency = newBase;
+                        vm.currencies = response.data;
+                        vm.total = 0;
+                        for (var item in vm.budget) {
+                            if (vm.budget[item].currency == vm.baseCurrency) {
+                                vm.total += vm.budget[item].cost * vm.budget[item].quantity;
+                            }
+                            else {
+                                vm.total += vm.budget[item].cost * vm.budget[item].quantity / vm.currencies.rates[vm.budget[item].currency];
+                            }
+                        }
+                    }, function error(response) {
+                        console.log(response);
+                });
             }
-        }, function error(response) {
-            console.log(response);
-    });
+        }
 
-    vm.updateCurrency = function updateCurrency(newBase){
-        if (newBase) {
-            CurrencyService.getRates(newBase)
-                .then(function success(response) {
-                    vm.baseCurrency = newBase;
-                    vm.currencies = response.data;
-                    vm.total = 0;
-                    for (var item in vm.budget) {
-                        if (vm.budget[item].currency == vm.baseCurrency) {
-                            vm.total += vm.budget[item].cost * vm.budget[item].quantity;
-                        }
-                        else {
-                            vm.total += vm.budget[item].cost * vm.budget[item].quantity / vm.currencies.rates[vm.budget[item].currency];
-                        }
+        vm.newCurrency = function newCurrency(currency) {
+            alert("Sorry, that currency is not supported.")
+        }
 
-                    }
-                }, function error(response) {
-                    console.log(response);
+        vm.querySearch = function querySearch (query) {
+            return query ? vm.currencyAutoComplete.filter( createFilterFor(query) ) : vm.currencyAutoComplete;
+        }
+
+        function createFilterFor(query) {
+            var uppercaseQuery = angular.uppercase(query);
+            return function filterFn(currency) {
+                return (currency.value.indexOf(uppercaseQuery) === 0);
+            }
+        }
+
+        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+        $scope.editActivity = function(ev, activityIn) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+            var activity = {};
+            var edit = false;
+            if (activityIn) {
+                console.log(activityIn);
+                activity = activityIn;
+                console.log(activity);
+                edit = true;
+            }
+
+            $mdDialog.show({
+                controller: DialogController,
+                controllerAs: 'dvm',
+                templateUrl: 'static/app/new-item/new-item.template.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen,
+                locals: {
+                    edit: edit,
+                    activity: activity
+                }
+            })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
             });
-        }
 
+            $scope.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function(wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
     }
-
-    vm.newCurrency = function newCurrency(currency) {
-        alert("Sorry, that currency is not supported.")
-    }
-
-    vm.querySearch = function querySearch (query) {
-        return query ? vm.currencyAutoComplete.filter( createFilterFor(query) ) : vm.currencyAutoComplete;
-    }
-
-    function createFilterFor(query) {
-        var uppercaseQuery = angular.uppercase(query);
-        return function filterFn(currency) {
-            return (currency.value.indexOf(uppercaseQuery) === 0);
-        }
-    }
-
-  $scope.status = '  ';
-  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-
-
-    $scope.editActivity = function(ev, activityIn) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-    var activity = {};
-    var edit = false;
-    if (activityIn) {
-        console.log(activityIn);
-        activity = activityIn;
-        console.log(activity);
-        edit = true;
-    }
-
-    $mdDialog.show({
-      controller: DialogController,
-      controllerAs: 'dvm',
-      templateUrl: 'static/app/new-item/new-item.template.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true,
-      fullscreen: useFullScreen,
-      locals: {
-        edit: edit,
-        activity: activity
-      }
-    })
-    .then(function(answer) {
-        $scope.status = 'You said the information was "' + answer + '".';
-      }, function() {
-        $scope.status = 'You cancelled the dialog.';
-    });
-
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
-
-  };
-
-  }
 
 })();
 
@@ -226,22 +156,22 @@ DialogController.$inject = [
 ]
 
 function DialogController($mdDialog, locals) {
-  var vm = this;
-  vm.edit = locals.edit;
-  vm.activity = locals.activity;
-  console.log(vm.activity);
-  vm.hide = function() {
-    $mdDialog.hide();
-  };
+    var vm = this;
+    vm.edit = locals.edit;
+    vm.activity = locals.activity;
+    console.log(vm.activity);
+    vm.hide = function() {
+        $mdDialog.hide();
+    };
 
-  vm.cancel = function() {
-    $mdDialog.cancel();
-  };
+    vm.cancel = function() {
+        $mdDialog.cancel();
+    };
 
-  vm.add = function(answer) {
-    $mdDialog.hide(answer);
-  };
-  vm.update = function(activity) {
-    $mdDialog.hide(activity);
-  };
+    vm.add = function(answer) {
+        $mdDialog.hide(answer);
+    };
+    vm.update = function(activity) {
+        $mdDialog.hide(activity);
+    };
 }
