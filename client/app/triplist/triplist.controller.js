664b72d3fdf340f3a5145c2ce43b23536f4f6c58
emilyls <snyderem@oregonstate.edu>
2016-07-19 18:04:52 -0700

    displays trips from db

--- client/app/triplist/triplist.controller.js
+++ client/app/triplist/triplist.controller.js
@@ -1,86 +1,86 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('travelcal.triplist')
-    .controller('TriplistController', TriplistController);
+    angular
+        .module('travelcal.triplist')
+        .controller('TriplistController', TriplistController);
 
-  TriplistController.$inject = [
-    '$http',
-    '$scope',
-    '$mdDialog',
-    '$mdMedia',
-    'TripService'
-  ];
+    TriplistController.$inject = [
+        '$http',
+        '$scope',
+        '$mdDialog',
+        '$mdMedia',
+        'TripService'
+    ];
 
-  function TriplistController($http, $scope, $mdDialog, $mdMedia, TripService) {
-    var vm = this;
-    vm.trips = [{country: 'Denmark', arrive: '9/28/2016', depart: '10/10/2016'},
-                {country: 'Spain', arrive: '6/2/2017', depart: '6/15/2017'},
-                {country: 'Australia', arrive: '12/28/2017', depart: '1/20/2018'}]
+    function TriplistController($http, $scope, $mdDialog, $mdMedia, TripService) {
+        var vm = this;
+
+        var updateTrips = function updateTrips(status, message) {
+            if (status) {
+                vm.trips = message.data;
+            }
+        };
+        TripService.list(updateTrips);
+
+        $scope.status = '  ';
+        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
 
-  $scope.status = '  ';
-  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+        $scope.newTrip = function(ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
 
+            var addTrip = function addTrip(status, message) {
+                if (status) {
+                    TripService.list(updateTrips);
+                }
+            };
 
-  $scope.newTrip = function(ev) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'new-trip.template.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:false,
+                fullscreen: useFullScreen
+            })
+            .then(function(answer) {
+                $scope.tripStart = JSON.stringify(answer.start);
+                $scope.tripStart = $scope.tripStart.slice(1,11);
 
-    var trips = function updateTrips(status, message) {
-        console.log(message)
+                $scope.tripEnd = JSON.stringify(answer.end);
+                $scope.tripEnd = $scope.tripEnd.slice(1,11);
+
+                $scope.tripName = answer.name;
+
+                TripService.create($scope.tripName, $scope.tripStart, $scope.tripEnd, addTrip);
+
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+
+            $scope.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function(wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
     }
 
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'new-trip.template.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:false,
-      fullscreen: useFullScreen
-    })
-    .then(function(answer) {
-        $scope.tripStart = JSON.stringify(answer.start);
-        $scope.tripStart = $scope.tripStart.slice(1,11);
-        $scope.tripEnd = JSON.stringify(answer.end);
-        $scope.tripEnd = $scope.tripEnd.slice(1,11);
-        $scope.tripName = answer.name;
-        console.log($scope.tripStart);
-        console.log($scope.tripEnd);
+    function DialogController($scope, $mdDialog) {
 
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
 
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
 
-      TripService.create($scope.tripName, $scope.tripStart, $scope.tripEnd, trips);
-
-    }, function() {
-        $scope.status = 'You cancelled the dialog.';
-    });
-
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
-
-  };
-
-  }
-
-  function DialogController($scope, $mdDialog) {
-//  $scope.NewItemController = NewItemController;
-  $scope.hide = function() {
-    $mdDialog.hide();
-  };
-
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
-
-  $scope.answer = function(answer) {
-    $mdDialog.hide(answer);
-  };
-}
-
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 
 })();
 
