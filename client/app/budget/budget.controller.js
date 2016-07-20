be20e26c7bd8a0b59abb51981a4504fba5c2f8dd
Jens Bodal <jensbodal@users.noreply.github.com>
2016-07-19 21:26:28 -0700

    added promise return if error

--- client/app/budget/budget.controller.js
+++ client/app/budget/budget.controller.js
@@ -20,8 +20,9 @@
                 url:'http://api.fixer.io/latest?base=' + base
             }).success(function(data) {
                 return data;
-            }).error(function() {
-                console.log("error");
+            }).error(function(response) {
+                console.log(response);
+                return response;
             })
         }
 
