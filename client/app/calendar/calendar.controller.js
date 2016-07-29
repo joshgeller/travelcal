819d977f2849494617b67727f2657c07a724d8e9
emilyls <snyderem@oregonstate.edu>
2016-07-28 21:33:39 -0700

    can add events to calendar

--- client/app/calendar/calendar.controller.js
+++ client/app/calendar/calendar.controller.js
@@ -7,14 +7,16 @@
 
     CalendarController.$inject = [
         '$scope',
+        '$mdDialog',
+        '$mdMedia',
+        '$compile',
+        'uiCalendarConfig',
         '$location',
         'TripService',
-        'CalendarService',
-        '$compile',
-        'uiCalendarConfig'
+        'CalendarService'
     ];
 
-    function CalendarController($scope, $location, TripService, CalendarService, $compile, uiCalendarConfig) {
+    function CalendarController($scope, $mdDialog, $mdMedia, $compile, uiCalendarConfig, $location, TripService, CalendarService) {
         var vm = this;
 
 //       var  tripId = $location.search().tripId || -1;
@@ -22,40 +24,48 @@
         if (tripId > -1) {
             $scope.eventSources = [];
 
-
             TripService.retrieve(tripId, function(result, response) {
                 if (result) {
                     vm.trip = response.data;
                     vm.title = vm.trip.name;
-                    console.log(vm.trip);
+                    vm.tripStart = new Date(vm.trip.start_date);
+                    vm.tripEnd = new Date(vm.trip.end_date);
+                    $scope.eventSources.push({color: '#f00', events: [{title: "TRIP START", start: vm.tripStart}, {title: "TRIP END", start: vm.tripEnd}]});
                     vm.calendar = response.data.calendar;
                     if (vm.calendar.data) {
                         $scope.events = convertCalendarData(vm.calendar.data);
-                        console.log($scope.events);
+                        $scope.eventSources.push($scope.events);
+                        console.log($scope.eventSources);
                     }
-                    else {
-                        $scope.events = [];
-                    }
-                    $scope.eventSources.push($scope.events);
                 }
                 else {
                     console.log("INVALID TRIP ID");
                 }
             });
 
+            $scope.addEvent = function() {
+                $scope.events.push({
+                    title: 'Open Sesame',
+                    start: new Date(y, m, 28),
+                    end: new Date(y, m, 29),
+                    className: ['openSesame']
+                });
+            };
 
-            /* alert on eventClick */
-            $scope.alertOnEventClick = function( date, jsEvent, view){
-                $scope.alertMessage = (date.title + ' was clicked ');
+            /* config object */
+            $scope.uiConfig = {
+                calendar:{
+                    height: 450,
+                    editable: true,
+                    header:{
+                        left: 'title',
+                        center: '',
+                        right: 'today prev,next'
+                    },
+                    eventRender: $scope.eventRender
+                }
             };
-            /* alert on Drop */
-             $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
-               $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
-            };
-            /* alert on Resize */
-            $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
-               $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
-            };
+
             /* add and removes an event source of choice */
             $scope.addRemoveEventSource = function(sources,source) {
               var canAdd = 0;
@@ -68,15 +78,6 @@
               if(canAdd === 0){
                 sources.push(source);
               }
-            };
-            /* add custom event*/
-            $scope.addEvent = function() {
-              $scope.events.push({
-                title: 'Open Sesame',
-                start: new Date(y, m, 28),
-                end: new Date(y, m, 29),
-                className: ['openSesame']
-              });
             };
             /* remove event */
             $scope.remove = function(index) {
@@ -94,27 +95,79 @@
             };
              /* Render Tooltip */
             $scope.eventRender = function( event, element, view ) {
-                element.attr({'tooltip': event.title,
-                             'tooltip-append-to-body': true});
+                element.attr({'tooltip': event.title, 'tooltip-append-to-body': true});
                 $compile(element)($scope);
             };
-            /* config object */
-            $scope.uiConfig = {
-              calendar:{
-                height: 450,
-                editable: true,
-                header:{
-                  left: 'title',
-                  center: '',
-                  right: 'today prev,next'
-                },
-                eventClick: $scope.alertOnEventClick,
-                eventDrop: $scope.alertOnDrop,
-                eventResize: $scope.alertOnResize,
-                eventRender: $scope.eventRender
-              }
-            };
+
         }
+
+        var updateCalendar = function updateCalendar(status, message) {
+            if (status) {
+                vm.calendar.data = message.data.data;
+            }
+        };
+
+        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+
+        $scope.editActivity = function(ev, activityIn, keyIn) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+            var activity = {};
+            var edit = false;
+            if (activityIn) {
+                angular.copy(activityIn, activity);
+                edit = true;
+            }
+
+            $mdDialog.show({
+                controller: DialogController,
+                controllerAs: 'dvm',
+                templateUrl: 'static/app/new-item/new-item.template.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen,
+                locals: {
+                    edit: edit,
+                    activity: activity
+                }
+            })
+            .then(function(activity) {
+
+                if (typeof activity == 'boolean' && activity == true) {
+                    if (keyIn > -1) {
+                        vm.calendar.data.splice(keyIn, 1);
+                    }
+                }
+                else {
+                    if (!activity.quantity){
+                        activity.quantity = 1;
+                    }
+                    if (vm.calendar.data == null) {
+                        vm.calendar.data = {};
+                    }
+                    if (keyIn != null) {
+                        vm.calendar.data[keyIn] = activity;
+                        $scope.eventSources.events[keyIn] = convertCalendarData([activity])[0];
+                    }
+                    else {
+                        vm.calendar.data.push(activity);
+                        $scope.eventSources[1].push(convertCalendarData([activity])[0]);
+                    }
+                }
+
+                CalendarService.update(vm.calendar.id, vm.calendar.data, updateCalendar);
+
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+
+            $scope.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function(wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+
 
     }
 
@@ -124,9 +177,18 @@ function convertCalendarData(events) {
     formattedEvents = [];
 
     for (var i = 0; i < events.length; i++) {
-        events[i].start = new Date(events[i].startsAt);
-        events[i].end  = new Date(events[i].endsAt);
-        formattedEvents.push(events[i]);
+        console.log(events[i]);
+        if (events[i].hasOwnProperty("startsAt") && events[i].startsAt != null) {
+            events[i].start = new Date(events[i].startsAt);
+        }
+        if (events[i].hasOwnProperty("endsAt") && events[i].endsAt != null) {
+            console.log("ends at");
+            events[i].end  = new Date(events[i].endsAt);
+        }
+        if (events[i].start) {
+            formattedEvents.push(events[i]);
+        }
+
     }
 
     return formattedEvents;
