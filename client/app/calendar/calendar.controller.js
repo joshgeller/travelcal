1c329e28bb6b28b7be71bc01d5e09daa16acbff1
emilyls <snyderem@oregonstate.edu>
2016-07-28 18:41:07 -0700

    minor change to calendar.js and add calendar to template

--- client/app/calendar/calendar.controller.js
+++ client/app/calendar/calendar.controller.js
@@ -6,83 +6,31 @@
         .controller('CalendarController', CalendarController);
 
     CalendarController.$inject = [
-        '$http',
         '$scope',
-        '$location',
-        '$mdDialog',
-        '$mdMedia',
-        'TripService'
     ];
 
-    function CalendarController($http, $scope, $location, $mdDialog, $mdMedia, TripService) {
+    function CalendarController($scope) {
         var vm = this;
-        vm.loadTrip = loadTrip;
-        vm.trips = [];
-    
-        function loadTrip(trip) {
-            $location.path('/budget').search({tripId: trip.id});
-        }
 
-        var updateTrips = function updateTrips(status, message) {
-            if (status) {
-                vm.trips = message.data;
+        vm.eventSources = [];
+        vm.uiConfig = {
+            calendar: {
+                height:450,
+                editable: true,
+                header: {
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev, next'
+                },
+                eventClick: vm.alertEventOnClick,
+                eventDrop: vm.alertOnDrop,
+                eventResize: vm.alertOnResize
             }
         };
-        TripService.list(updateTrips);
-
-        $scope.status = '  ';
-        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
 
-        $scope.newTrip = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-
-            var addTrip = function addTrip(status, message) {
-                if (status) {
-                    TripService.list(updateTrips);
-                }
-            };
-
-            $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'new-trip.template.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose:false,
-                fullscreen: useFullScreen
-            })
-            .then(function(answer) {
-                $scope.tripStart = JSON.stringify(answer.start);
-                $scope.tripStart = $scope.tripStart.slice(1,11);
-
-                $scope.tripEnd = JSON.stringify(answer.end);
-                $scope.tripEnd = $scope.tripEnd.slice(1,11);
-
-                $scope.tripName = answer.name;
-
-                TripService.create($scope.tripName, $scope.tripStart, $scope.tripEnd, addTrip);
-
-            }, function() {
-                $scope.status = 'No trip was added.';
-            });
-
-            $scope.$watch(function() {
-                return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
-                $scope.customFullscreen = (wantsFullScreen === true);
-            });
-        };
     }
 
-    function DialogController($scope, $mdDialog) {
-        $scope.cancel = function() {
-            $mdDialog.cancel();
-        };
-
-        $scope.add = function(answer) {
-            $mdDialog.hide(answer);
-        };
-    }
 
 })();
 
