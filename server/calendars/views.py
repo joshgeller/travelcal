c271c08412481abae11d5b2a4308b79142335d26
Josh Geller <josh@covalentcareers.com>
2016-08-07 13:42:22 -0400

    Finish popular activities backend

--- server/calendars/views.py
+++ server/calendars/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
@@ -17,23 +19,37 @@ class CalendarViewSet(viewsets.ModelViewSet):
         """
         Only return Calendars associated with the current user.
         """
-        account = self.request.user
-        return Calendar.objects.filter(trip__account=account)
+        return Calendar.objects.all()
 
     @list_route(methods=['get'])
     def popular(self, request, pk=None, *args, **kwargs):
         queryset = Calendar.objects.all()
         popular_activities = {}
         for calendar in queryset:
-            for activity in calendar.data:
-                if activity.address:
-                    if activity.address not in popular_activities:
-                        popular_activities.update({
-                            activity.address: 1
-                        })
-                    else:
-                        popularity = popular_activities.get(activity.address)
-                        popular_activities.update({
-                            activity.address: popularity + 1
-                        })
-        return Response(popular_activities)
+            if calendar.data:
+                for activity in calendar.data:
+                    addr = activity.get('address', {}).get('name', None)
+                    if addr:
+                        if addr not in popular_activities:
+                            popular_activities.update({
+                                addr: 1
+                            })
+                        else:
+                            popularity = popular_activities.get(addr)
+                            popular_activities.update({
+                                addr: popularity + 1
+                            })
+        results = []
+        for name, score in popular_activities.items():
+            results.append(
+                {
+                    'title': name,
+                    'address': {
+                        'name': name
+                    },
+                    'score': score,
+                    'start': datetime.datetime.now(),
+                    'end': datetime.datetime.now()
+                }
+            )
+        return Response(results)
