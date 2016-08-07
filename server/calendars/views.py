3dacd2b53e71b862d13a6f5996a69266d31349e2
Josh Geller <josh@covalentcareers.com>
2016-08-07 13:27:29 -0400

    Popular activities backend

--- server/calendars/views.py
+++ server/calendars/views.py
@@ -1,4 +1,6 @@
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from .models import Calendar
 from .serializers import CalendarSerializer
@@ -15,11 +17,23 @@ class CalendarViewSet(viewsets.ModelViewSet):
         """
         Only return Calendars associated with the current user.
         """
-        # TODO: Remove the try/except block to restrict Calendar to
-        # the current user only. This is disabled for midpoint check
-        # demonstration purposes only.
-        try:
-            account = self.request.user
-            return Calendar.objects.filter(trip__account=account)
-        except TypeError:
-            return Calendar.objects.all()
+        account = self.request.user
+        return Calendar.objects.filter(trip__account=account)
+
+    @list_route(methods=['get'])
+    def popular(self, request, pk=None, *args, **kwargs):
+        queryset = Calendar.objects.all()
+        popular_activities = {}
+        for calendar in queryset:
+            for activity in calendar.data:
+                if activity.address:
+                    if activity.address not in popular_activities:
+                        popular_activities.update({
+                            activity.address: 1
+                        })
+                    else:
+                        popularity = popular_activities.get(activity.address)
+                        popular_activities.update({
+                            activity.address: popularity + 1
+                        })
+        return Response(popular_activities)
