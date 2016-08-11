c91775958ca8ba9bc5dbf125cbbb01b1f8db1657
jensbodal <jensbodal@gmail.com>
2016-08-11 10:10:26 -0700

    fix register error alert

--- client/app/register/register.controller.js
+++ client/app/register/register.controller.js
@@ -18,7 +18,9 @@
     vm.alerts = [];
     vm.forms = {};
     vm.register = function() {
-      while(vm.alerts.length) vm.alerts.pop();
+      while(vm.alerts.length) {
+        vm.alerts.pop();
+      }
       if (vm.forms.registerForm.$invalid) return;
       return $http.post('/api/v1/accounts/', {
         password: vm.password,
@@ -32,18 +34,15 @@
         $state.go('travelcal.home');
       }, function(res) {
         if (res.status > -1 && res.data.detail) {
-          // Iterate through invalid fields
-          for (var field in res.data.detail) {
-            // Display all alerts for this field
-            for (var err of res.data.detail[field]) {
-              vm.alerts.push({
-                message: err,
-                type: config.alerts.ERROR
-              });
-            }
-          }
+          // Iterate through invalid fields, res.data.detail is a string, not sure what it
+          // looks like if there are more than one error response, but this seems to work for 
+          // the one error message we get
+            vm.alerts.push({
+              message: res.data.detail,
+              type: config.alerts.ERROR
+            });
         }
-      })
+      });
     }
   }
 
