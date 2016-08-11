c98f72c416511ab15a67f969bef9a82c4097504f
emilyls <snyderem@oregonstate.edu>
2016-08-11 14:53:47 -0700

    update form set up, API call not working

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -6,12 +6,30 @@
     .controller('ProfileController', ProfileController);
 
   ProfileController.$inject = [
-    '$localStorage'
+    '$localStorage',
+    '$http'
   ];
 
-  function ProfileController($localStorage) {
+  function ProfileController($localStorage, $http) {
     var vm = this;
-    vm.pageTitle = 'Dynamic Page Title';
+    vm.newPassword = '';
+
+    vm.update = function () {
+       $http.post('/api/v1/accounts/', {
+        email: vm.username,
+        password: vm.newPassword
+      })
+      .then(function(res) {
+        console.log(res);
+        vm.alerts.push({
+          message: 'Account created. You may now log in.',
+          type: config.alerts.SUCCESS
+        });
+      }, function(res) {
+        console.log(res);
+      })
+    }
+
 
     vm.username = $localStorage.authenticatedUser.username;
   }
